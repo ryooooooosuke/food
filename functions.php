afe421774b4630b398659a25b1d287613fa22f69
ryooooooosuke <ryosuke.git@gmail.com>
2025-08-10 02:03:12 +0900

    particlesの導入

--- functions.php
+++ functions.php
@@ -13,11 +13,24 @@
 /**
  * 子テーマでのファイルの読み込み
  */
-add_action('wp_enqueue_scripts', function() {
-	
-	$timestamp = date( 'Ymdgis', filemtime( get_stylesheet_directory() . '/style.css' ) );
-	wp_enqueue_style( 'child_style', get_stylesheet_directory_uri() .'/style.css', [], $timestamp );
+add_action('wp_enqueue_scripts', function () {
 
-	/* その他の読み込みファイルはこの下に記述 */
-
+	$timestamp = date('Ymdgis', filemtime(get_stylesheet_directory() . '/style.css'));
+	wp_enqueue_style('child_style', get_stylesheet_directory_uri() . '/style.css', [], $timestamp);
+	/* その他の読み込みファイルはこの下に記述 */	// トップページのみparticles.jsを読み込み
+	if (is_front_page()) {
+		wp_enqueue_script('particles-js', get_stylesheet_directory_uri() . '/js/particles/particles.js', [], '2.0.0', true);
+		wp_enqueue_script('particles-app', get_stylesheet_directory_uri() . '/js/particles/app.js', ['particles-js'], '1.0.0', true);
+		wp_enqueue_style('particles-css', get_stylesheet_directory_uri() . '/css/particles.css', [], '1.0.0');
+	}
 }, 11);
+
+
+/**
+ * トップページのbodyタグ直下にparticles-jsのdivを追加
+ */
+add_action('wp_body_open', function () {
+	if (is_front_page()) {
+		echo '<div id="particles-js"></div>';
+	}
+});
